46c6a5fc5cecbde165485c64764f9d1d2c9856a9
Leonardo-Nascimento <leonardot.i@hotmail.com>
2018-10-22 00:09:44 -0300

    inicio da criação da tela de login

--- js/app.js
+++ js/app.js
@@ -14,3 +14,10 @@ document.addEventListener('DOMContentLoaded', function () {
   var instances = M.Sidenav.init(elems, {});
 
 });
+
+//$(window).load(function(){
+$(document).ready(function(){
+  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+  $('#modal1').modal();
+  $('#modal1').modal('open'); 
+});
