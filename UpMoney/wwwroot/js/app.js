18d7fbb064b2a7059712a6971771e4ed92dfb8ea
Leonardo-Nascimento <leonardot.i@hotmail.com>
2018-10-29 01:23:16 -0300

    Tela inicial em progresso, criação do menu

---
+++ UpMoney/wwwroot/js/app.js
@@ -0,0 +1,30 @@
+
+//$(".dropdown-trigger").dropdown();
+
+
+
+//function callParallax() {
+//  $('.parallax').parallax();
+//}
+
+//window.load = callParallax();
+
+////$(window).load(function(){
+//$(document).ready(function(){
+//  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+//  $('#modal1').modal();
+//  $('#modal1').modal('open'); 
+//});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+
+});
+
+
+//document.addEventListener('DOMContentLoaded', function () {
+//    var elems = document.querySelectorAll('.parallax');
+//    var instances = M.Parallax.init(elems, {});
+//});
+
